6bb26f3713b63daeee85ddc73b3843752a330c1c
Sam Shearer <sshearer101@gmail.com>
2021-09-20 21:23:32 -0600

    commit tree 186965818e588f36a047d8ea5ecbfba2c1732765

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else 
+      render json: { error: "plant not found" }, status: :not_found
+  end
+end
+
+def destroy
+  plant = Plant.find_by(id: params[:id])
+  if plant
+    plant.destroy
+  else 
+    render json: { error: "plant not found" }, status: :not_found
+  end
+end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+ 
 end
